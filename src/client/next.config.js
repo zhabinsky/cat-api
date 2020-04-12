f05a5526f6aed879c85c226061ff6bb5e4867723
Vladislav Zhabinsky <vladislavzhabinsky@gmail.com>
2020-04-12 23:58:41 +0300

    setup bundle analyzer

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -1,7 +1,11 @@
-const env = require('dotenv').config().parsed;
+const env = require('dotenv').config().parsed || {};
 
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
     poweredByHeader: false,
     compress: false, // offload load from the Node.js process, let nginx do that
     env,
-};
+});
